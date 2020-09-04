2698fbd89d57943041fbb5b95c8eac411e711acc
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-04 03:32:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,9 +4,11 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
- birthday_kids.each do |kids_name, age|
-   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+def happy_birthday(birthday_kids) #<--- defines a method of happy_birthda, that takes an argument of birthday_kids
+ 
+ birthday_kids.each do |kids_name, age| #<--- uses the each method attached to the birthday_kids hash to iterate through the double pipes with the arguments, kids_name and age. 
+   
+   puts "Happy Birthday #{kids_name}! You are now #{age} years old!" #<--- uses string interpolation to insert kids name and age into string.
  end
 end
 
